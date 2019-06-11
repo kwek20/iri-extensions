eedf6af9085c8d1d4535967b771c38cc0983ffee
muXxer <mux3r@web.de>
2019-06-11 23:39:45 +0200

    Add checksum and amount in the header

--- export-spent/index.js
+++ export-spent/index.js
@@ -16,50 +16,57 @@ var log = org.slf4j.LoggerFactory.getLogger(iri.IXI.class);
 curl http://localhost:14265 -X POST -H 'X-IOTA-API-Version: 1.4.1' -H 'Content-Type: application/json' -d '{"command": "Spent.generateSpentAddressesFile"}'
 */
 function generate(request) {
-	var file = new java.io.File(fileName);
+    var file = new java.io.File(fileName);
     if (file.exists()){
-    	if (file.isDirectory()){
-	    	log.error("Found a directory called {}, aborting!");
-	    	return ErrorResponse.create("Failed to create spent address due to {} beeing a folder", fileName);
-	    } else {
-	    	log.info("{} already exists.. Overwriting!", fileName);
-	    }
+        if (file.isDirectory()){
+            log.error("Found a directory called {}, aborting!");
+            return ErrorResponse.create("Failed to create spent address due to {} beeing a folder", fileName);
+        } else {
+            log.info("{} already exists.. Overwriting!", fileName);
+        }
     } else {
-    	file.createNewFile();
+        file.createNewFile();
     }
-    
-    // False for always overwriting
-    var writer = new java.io.FileWriter(file, false); 
 
-    var hashes = spentAddressProvider.getAllAddresses();
+    // False for always overwriting
+    var writer = new java.io.FileWriter(file, false);
+
     var separator = System.getProperty("line.separator");
-    
+
     // Start with the time
     writer.write(System.currentTimeMillis() + separator);
+
+    var hashes = spentAddressProvider.getAllAddresses();
 
     // Create a digest
     var digest = java.security.MessageDigest.getInstance("SHA-256");
     for (var i=0; i<hashes.length; i++){
-    	var hash = hashes[i].toString();
+        // Update with hash bytes
+        digest.update(hashes[i].bytes());
+    }
 
-    	writer.write(hash + separator);
-    	
-    	// Update with hash bytes
-    	digest.update(hash.getBytes());
-    	
-    	if (i % 100 === 99) {
-    		writer.flush;
-    	}
+    // Write checksum
+    var checksum = java.lang.String.format("%064x", new java.math.BigInteger(1, digest.digest()));
+    writer.write(checksum + separator);
+
+    // Write amount of addresses
+    writer.write(hashes.length.toString() + separator);
+
+    // Write spent addresses
+    for (var i=0; i<hashes.length; i++){
+        var hash = hashes[i].toString();
+
+        writer.write(hash + separator);
     }
 
     writer.close();
 
     return Response.create({
-		amount: hashes.length,
-		fileName: fileName,
-		sizeInKb: new java.lang.Integer(file.length() / (1024 * 1024)),
-		checksum: java.lang.String.format("%064x", new java.math.BigInteger(1, digest.digest()))
-	});
+        amount: hashes.length,
+        fileName: fileName,
+        sizeInKb: new java.lang.Integer(file.length() / (1024 * 1024)),
+        checksum: checksum
+    });
 }
 
 API.put("generateSpentAddressesFile", new Callable({ call: generate }))
